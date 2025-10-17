d8cf781e00f1fc84ac8e5dfb7732b1ff9078e088
Nagmani Jha <nagmanijha31@gmail.com>
2025-10-17 16:16:01 +0530

    feat: add authentication middleware to get-review route

--- src/routes/ai.routes.js
+++ src/routes/ai.routes.js
@@ -1,9 +1,10 @@
 // routes/ai.routes.js
 import express from 'express';
 import { getReview } from '../controllers/ai.controller.js';
-
+import { isauth } from '../middleware/userAuth.js';
 const router = express.Router();
 
-router.post("/get-review", getReview);
+
+router.post("/get-review", isauth, getReview);
 
 export default router;
